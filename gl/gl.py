3284c069846c073199a7bd25210d480e5cf6bde7
WooDose <59982861+WooDose@users.noreply.github.com>
2020-07-20 20:08:39 -0600

    Update gl.py
    Now with line drawing.

--- gl/gl.py
+++ gl/gl.py
@@ -27,7 +27,7 @@ class Render(object):
         pass
     
     def glCreateWindow(self):
-        print(self.framebuffer)
+        #print(self.framebuffer)
         self.framebuffer = [
             [self.clearColor for x in range(self.width)]
              for y in range(self.height)
@@ -55,13 +55,71 @@ class Render(object):
 
     def point(self,x,y):
         self.framebuffer[x][y] = self.drawColor
+    
+    def line(self, x1, y1, x2, y2):
+        dy = abs(y2 - y1)
+        #print(y2,y1)
+        dx = abs(x2 - x1)
+        # print(x2,x1)
+        # print(dx)
+        steep = dy > dx
+
+        if steep:
+            x1, y1 = y1, x1
+            x2, y2 = y2, x2
+
+        if x1 > x2:
+            x1, x2 = x2, x1
+            y1, y2 = y2, y1
+
+        # print(x1,x2,y1,y2)
+
+        offset =  0 * 2 * dx
+        threshold = 0.5 * 2 * dx
+        y = y1
+
+        points = []
+        # print( x1,x2)
+        for x in range(x1, x2 + 1):
+            # print (x, y)
+            if steep:
+                points.append((y, x))
+            else:
+                points.append((x, y))
+            
+            #print(dy)
+            offset += (dx if steep else dy) * 2
+            # print(offset, threshold, y )
+            if offset > threshold:
+                y += 1 if y1 < y2 else -1
+                threshold += 1 * 2 * dx
+
+        for point in points:
+            self.point(point[0], point[1])
 
     def glVertex(self, x,y): 
         xW = int(((x+1)*(self.ViewportWidth/2))+self.xNormalized)
+        #print(xW)
         yW = int(((y+1)*(self.ViewportHeight/2))+self.yNormalized)
         xW = (xW - 1) if xW == self.width else xW
         yW = (yW - 1) if yW == self.height else yW
         self.point(xW, yW)
+
+    def glLine(self, x0,y0,x1,y1):
+        x0W = ((x0+1)*(self.ViewportWidth/2))+self.xNormalized
+        # print(x0W)
+        x0W = int(x0W)
+        x1W = ((x1+1)*(self.ViewportWidth/2))+self.xNormalized
+        # print(x1W)
+        x1W = int(x1W)
+        y0W = int(((y0+1)*(self.ViewportHeight/2))+self.yNormalized)
+        y1W = int(((y1+1)*(self.ViewportHeight/2))+self.yNormalized)
+        # print(x0W, x1W, y0W, y1W)
+        x0W = (x0W - 1) if x0W == self.width else x0W
+        x1W = (x1W - 1) if x1W == self.width else x1W
+        y0W = (y0W - 1) if y0W == self.height else y0W
+        y1W = (y1W - 1) if y1W == self.height else y1W
+        self.line(x0W, y0W, x1W, y1W)
 
     def glFinish(self, filename):
         f = open(filename, 'bw')
@@ -103,7 +161,7 @@ class Render(object):
 
         # Pixel data
         
-        print(self.framebuffer)
+        #print(self.framebuffer)
         for x in range(self.height):
             for y in range(self.width):
                 f.write(self.framebuffer[y][x])
@@ -115,9 +173,22 @@ class Render(object):
 ##Please for the love of God don't use non-4 multiples for your dimensions unless you want to absoultely do you know what to your you know what.
 
 bitmap = Render(80,80,80,80, 0, 0)
-bitmap.glColor(0, 0.5, 0.75)
 # for x in range(20, 30):
 #     for y in range(20, 30):
 #         bitmap.glVertex(x, y)
-bitmap.glVertex(1,1) 
-bitmap.glFinish(r'woodose.bmp')
+# bitmap.glVertex(0,0)
+bitmap.glColor(0, 0.5, 0.75)
+bitmap.glLine(-1, 0.25, 1, -0.25)
+bitmap.glColor(1, 0, 0)
+bitmap.glLine(1, 1, 0, 0)
+bitmap.glColor(0, 1, 0)
+bitmap.glLine(-1, -1, 0, 0)
+bitmap.glColor(1,1,1)
+bitmap.glLine(-1,0.33,0.33,-1)
+bitmap.glColor(0.25, 0.37, 0.86)
+bitmap.glLine(-1, 0, 1, 0)
+bitmap.glColor(0.75, 0.37, 0.86)
+bitmap.glLine(0, -1, 0, 1)
+bitmap.glColor(0.10, 0.10, 0.50)
+bitmap.glLine(0, 1, 0, -0)
+bitmap.glFinish(r'woodoseline.bmp')
